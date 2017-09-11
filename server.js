7abfae4d84ead6b2b572ae944d4bae5f24cdc4ba
biyunbo <252450041@qq.com>
2017-09-11 16:52:53 +0800

    router

--- server.js
+++ server.js
@@ -1,15 +1,60 @@
 var express = require('express');
-var app = express();
 
-var query = require('./utils');
+var app =express();
+var query = require('./utils')
 
-app.get('/', function(req, res) {
- 	res.send('hello world');
-}).listen(2024);
+app.get("/",function(req,res){
+	res.send('hello express')
+}).listen(2024,() => {
+	console.log('hello word')
+})
 
-app.get('/book',function(req, res){
-	query(`select * from books`, (err, results) => {
-    if (err) throw err
-    res.send(results)
-  })
-});
+//书本信息
+app.get('/books/:id',function(req,res){
+	var id = req.params.id;
+	query(`select * from books WHERE bookId=${id}`,(err, rows) => {
+	    res.json({data:rows[0],success:true})
+	})
+})
+
+//书本列表
+app.get('/booklist',function(req,res){
+	var type = req.query.type;
+	if(type){
+		if(type=="fan"){
+			type="玄幻"
+		}else if(type=="wan"){
+			type="修真"
+		}
+		else if(type=="city"){
+			type="都市"
+		}
+		else if(type=="ero"){
+			type="言情"
+		}
+		query(`SELECT * FROM booklist WHERE type='${type}' ORDER BY ratings DESC`,(err, rows) => {
+		    res.json({data:rows,success:true})
+		})
+	}else{
+		query(`SELECT * FROM booklist ORDER BY ratings DESC`,(err, rows) => {
+		    res.json({data:rows,success:true})
+		})
+	}
+	
+})
+
+//书本章节&&章节内容
+app.get('/books',function(req,res){
+	var id = req.query.id;
+	var chapter = req.query.chapter;
+	if(chapter){
+		query(`select * from books WHERE bookId=${id} and chapterId=${chapter}`,(err, rows) => {
+		    res.json({data:rows,success:true})
+		})
+	}else{
+		query(`select * from books WHERE bookId=${id} ORDER BY chapterId ASC`,(err, rows) => {
+		    res.json({data:rows,success:true})
+		})
+	}
+	
+})
--- utils.js
+++ utils.js
@@ -1,26 +1,20 @@
-var mysql = require('mysql')
-
+// 连接MySQL
+var mysql = require('mysql');
 var pool = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: '123456',
-	database: 'book',
-	port: 3306
-})
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'book',
+    port: 3306
+});
 
-const query = function (sql, option, callback) {
-  pool.getConnection(function (err, connection) {
-    // Use the connection
-    connection.query(sql, option, function (error, results, fields) {
-      // And done with the connection.
-      connection.release()
-
-      // Handle error after the release.
-      if (error) throw error
-      callback(err, results, fields)
-      // Don't use the connection here, it has been returned to the pool.
-    })
-  })
+function query(sql, callback) {
+    pool.getConnection(function (err, connection) {
+        // Use the connection
+        connection.query(sql, function (err, rows) {
+            callback(err, rows);
+            connection.release();//释放链接
+        });
+    });
 }
-
 module.exports = query
